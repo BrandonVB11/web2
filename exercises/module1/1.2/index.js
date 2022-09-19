1b219887729ce2efd5fbee0d8ae7d5838d7bfedd
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-09-19 11:58:19 +0200

    aaa

--- exercises/module1/1.2/index.js
+++ exercises/module1/1.2/index.js
@@ -2,7 +2,13 @@ const dateTimeNow = new Date();
 console.log(dateTimeNow.toLocaleDateString())
 console.log(dateTimeNow.toLocaleTimeString())
 
-    alert(
-        dateTimeNow.toLocaleDateString()+
-        "\n"+dateTimeNow.toLocaleTimeString()
-    );
+const thisMessage = "This is the best moment to have a look at this website !";
+
+function addDateTime (message){
+    var dateTime = dateTimeNow.toLocaleDateString()+
+    "\n"+dateTimeNow.toLocaleTimeString()+"\n";
+    return dateTime+message
+};
+
+
+    alert(addDateTime(thisMessage));
---
+++ exercises/module1/1.4/index.js
@@ -0,0 +1,10 @@
+const styleRed = document.getElementById("red");
+const styleOrange = document.getElementById("orange");
+const styleGreen = document.getElementById("green");
+
+var timeoutId;
+const delayInSeconds = 2;
+
+function setRed(){
+    
+}
