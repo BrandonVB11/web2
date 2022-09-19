b179d447ec1d6d0b0ec87b6b31f028c05c8840d0
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-09-19 12:10:34 +0200

    exo 1.5 debut

---
+++ exercises/module1/1.5/index.js
@@ -0,0 +1,19 @@
+var count = 0;
+var btn = document.querySelector("#btn");
+var disp = document.querySelector("#display");
+var span = document.querySelector("#phrase");
+  
+btn.onclick = function () {
+     count++;
+     disp.innerHTML = count;
+};
+
+btn.addEventListener("click", () => {
+    if(count >= 5 && count <= 9){
+        span.innerHTML = "Bravo, bel échauffement ! ";
+    };
+    if(count >= 10){
+        span.innerHTML = "Vous êtes passé maître en l'art du clic ! ";
+    };
+    console.log("onClickHandlerForBtn1::click");
+  });
