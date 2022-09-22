efbfce2d0df707d30868e62b706dfea682bd51c7
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-09-22 11:39:04 +0200

    End exo 1.5

--- exercises/module1/1.4/index.js
+++ exercises/module1/1.4/index.js
@@ -1,16 +1,44 @@
 const styleRed = document.getElementById("red");
-const styleOrange = document.querySelector("#orange");
-const styleGreen = document.querySelector("#green");
-var delay = 2;
+const styleOrange = document.getElementById("orange");
+const styleGreen = document.getElementById("green");
+var delay = 0;
 
 var myIntervalId;
 
 start();
 
 function start(){
-  myIntervalId = setInterval(changeColor,2);
+  clearInterval(myIntervalId);
+  myIntervalId = undefined;
+  myIntervalId = setInterval(changeColor,1000);
 };
 
 function changeColor() {
-    styleRed.document.body.style.background = "red";
+  delay++;
+  if(delay == 1){
+    styleOrange.style.backgroundColor = "white"
+    styleRed.style.backgroundColor = "red"
+    start();
+  }
+  if(delay == 2){
+    styleRed.style.backgroundColor = "white"
+    styleOrange.style.backgroundColor = "orange"
+    start();
+  }
+  if(delay == 3){
+    styleOrange.style.backgroundColor = "white"
+    styleGreen.style.backgroundColor = "green"
+    start();
+  }
+  if(delay == 4){
+    styleGreen.style.backgroundColor = "white"
+    styleOrange.style.backgroundColor = "orange"
+    start();
+  }
+  if(delay == 5){
+    styleOrange.style.backgroundColor = "white"
+    styleRed.style.backgroundColor = "red"
+    delay == 1;
+    start();
+  }
 };
--- exercises/module1/1.5/index.js
+++ exercises/module1/1.5/index.js
@@ -2,12 +2,14 @@ var count = 0;
 var btn = document.querySelector("#btn");
 var disp = document.querySelector("#display");
 var span = document.querySelector("#phrase");
+var btnImg = document.querySelector("#RGB");
 
-  
+
 btn.onclick = function () {
      count++;
      disp.innerHTML = count;
 };
+
 
 btn.addEventListener("click", () => {
     if(count >= 5 && count <= 9){
@@ -16,9 +18,18 @@ btn.addEventListener("click", () => {
     if(count >= 10){
         span.innerHTML = "Vous êtes passé maître en l'art du clic ! ";
     };
+    if(count >= 100){
+        span.innerHTML = "Tu es le maitre du clic ! ";
+    }
     console.log("onClickHandlerForBtn1::click");
   });
 
-function returnPicture (){
-    document.getElementById("rick").src ="Rick_roll.gif";
-}
+ 
+  function ramdom(number){
+    return Math.floor(Math.random()*(number+1));
+  }
+  
+  btnImg.addEventListener('click',() =>{ 
+    const color = `rgb(${ramdom(255)},${ramdom(255)},${ramdom(255)})`;
+    document.body.style.backgroundColor = color;
+});
