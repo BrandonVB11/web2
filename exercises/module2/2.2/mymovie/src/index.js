66c0dae0dcab5875034f2a6b051e36896e20d921
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-09-26 12:40:19 +0200

    myMovies : module 2 : dynamic homepage

--- exercises/module2/2.2/mymovie/src/index.js
+++ exercises/module2/2.2/mymovie/src/index.js
@@ -1,2 +1,63 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+const movies = [
+    {
+        id: 1,
+        title: "Red Notice",
+        diffusion: "16H40"
+    },
+    {
+        id: 2,
+        title: "Fast & Furions 10",
+        diffusion: "20h00"
+    }
+];
+
+
+function renderMenuFromString(menu) {
+    const menuTableAsString = getMenuTableAsString(menu);
+  
+    const main = document.querySelector('main');
+  
+    main.innerHTML += menuTableAsString;
+  }
+  
+  function getMenuTableAsString(menu) {
+    const menuTableLines = getAllTableLinesAsString(menu);
+    const menuTable = addLinesToTableHeadersAndGet(menuTableLines);
+    return menuTable;
+  }
+
+  function addLinesToTableHeadersAndGet(tableLines) {
+    const menuTable = `
+    <div class="table-responsive pt-5">
+      <table class="table table-danger">
+        <tr>
+          <th>Film</th>
+          <th>Description</th>
+          <th>D</th>
+        </tr>
+        ${tableLines}    
+      </table>
+    </div>
+    `;
+    return menuTable;
+  };
+
+  function getAllTableLinesAsString(menu) {
+    let menuTableLines = '';
+  
+    menu?.forEach((movie) => {
+      menuTableLines += `<tr>
+        <td>${movie.title}</td>
+        <td>${movie.content}</td>
+        <td>${movie.diffusion}</td>
+      </tr>`;
+    });
+  
+    return menuTableLines;
+  };
+
+  
+renderMenuFromString(movies);
