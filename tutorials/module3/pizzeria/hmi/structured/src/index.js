6a1d2b8e7d28af7d4d7ecf9566b37c1235bcf7f3
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-10-03 12:34:42 +0200

    myMovies : module 3 : refactoring & MooviePage

--- tutorials/module3/pizzeria/hmi/structured/src/index.js
+++ tutorials/module3/pizzeria/hmi/structured/src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
-import 'animate.css';
+
 
 import HomePage from './Components/Pages/HomePage';
 import Header from './Components/Header/Header';
