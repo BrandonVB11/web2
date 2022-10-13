bd22dd84152fe63b470e8a0fbbc7e38f8afc1462
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-10-13 12:17:11 +0200

    myMovies : module 4 : basic API

--- exercises/module4/4.1/Mymovies/app.js
+++ exercises/module4/4.1/Mymovies/app.js
@@ -3,9 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var moviesRouter = require('./routes/movies');
+
+var moviesRouter = require('./routes/films');
 
 var app = express();
 
@@ -14,8 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/films',moviesRouter);
 
 module.exports = app;
--- exercises/module4/4.1/Mymovies/routes/movies.js
+++ exercises/module4/4.1/Mymovies/routes/movies.js
@@ -25,6 +25,26 @@ const FILM = [
   },
 ];
 
+// Read all the films, filtered by minimum-duration if the query param exists
+router.get('/', function (req, res) {
+    const minimumFilmDuration = req?.query
+      ? parseInt(req.query['minimum-duration'])
+      : undefined;
+    if (
+      minimumFilmDuration &&
+      (typeof minimumFilmDuration !== 'number' || minimumFilmDuration <= 0)
+    )
+      return res.sendStatus(400);
+  
+    if (!minimumFilmDuration) return res.json(films);
+  
+    const filmsReachingMinimumDuration = films.filter(
+      (film) => film.duration >= minimumFilmDuration
+    );
+    return res.json(filmsReachingMinimumDuration);
+  });
+
+
 // Read all the movies from the menu
 router.get('/', (req, res, next) => {
   console.log('GET /films');
@@ -46,28 +66,74 @@ router.get('/:id', (req, res) => {
 // Create a film to be added to the menu.
 router.post('/', (req, res) => {
     const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
-    const duration = req?.body?.duration !== 0 ? req.body.duration : undefined;
-    const budget = req?.body?.budget !== 0 ? req.body.budget : undefined;
+    const link =req?.body?.content?.trim().length !== 0 ? req.body.link : undefined;
+    const duration =
+    typeof req?.body?.duration !== 'number' || req.body.duration < 0
+      ? undefined
+      : req.body.duration;
+  const budget =
+    typeof req?.body?.budget !== 'number' || req.body.budget < 0
+      ? undefined
+      : req.body.budget;
   
     console.log('POST /films');
   
-    if (!title || !duration || !budget) return res.sendStatus(400); // error code '400 Bad request'
+    if (!title || !link || !duration || !budget) return res.sendStatus(400); // error code '400 Bad request'
   
     const lastItemIndex = FILM?.length !== 0 ? FILM.length - 1 : undefined;
     const lastId = lastItemIndex !== undefined ? FILM[lastItemIndex]?.id : 0;
     const nextId = lastId + 1;
   
-    const newMovie = {
-      id: nextId,
-      title: title,
-      duration: duration,
-      budget:budget,
-      link:"https://kinepolis.be/fr/?main_section=tous%20les%20films",
+    const newFilm = { id: nextId, title, link, duration, budget };
+
+    FILM.push(newFilm);
+  
+    res.json(newFilm);
+});
+
+// Delete a film
+router.delete('/:id', function (req, res) {
+    const indexOfFilmFound = FILM.findIndex((film) => film.id == req.params.id);
+  
+    if (indexOfFilmFound < 0) return res.sendStatus(404);
+  
+    const itemsRemoved = FILM.splice(indexOfFilmFound, 1);
+    const itemRemoved = itemsRemoved[0];
+  
+    return res.json(itemRemoved);
+});
+
+// Update a film identified by its id
+router.patch('/:id', function (req, res) {
+    const title = req?.body?.title;
+    const link = req?.body?.link;
+    const duration = req?.body?.duration;
+    const budget = req?.body?.budget;
+  
+    if (
+      !req.body ||
+      (title && !title.trim()) ||
+      (link && !link.trim()) ||
+      (duration && (typeof req?.body?.duration !== 'number' || duration < 0)) ||
+      (budget && (typeof req?.body?.budget !== 'number' || budget < 0))
+    )
+      return res.sendStatus(400);
+  
+    const indexOfFilmFound = FILM.findIndex((film) => film.id == req.params.id);
+  
+    if (indexOfFilmFound < 0) return res.sendStatus(404);
+  
+    const filmPriorToChange = FILM[indexOfFilmFound];
+    const objectContainingPropertiesToBeUpdated = req.body;
+  
+    const updatedFilm = {
+      ...filmPriorToChange,
+      ...objectContainingPropertiesToBeUpdated,
     };
   
-    FILM.push(newMovie);
+    FILM[indexOfFilmFound] = updatedFilm;
   
-    res.json(newMovie);
-});
+    return res.json(updatedFilm);
+  });
 
 module.exports = router;
