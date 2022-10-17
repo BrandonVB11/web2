3a9ecd8ef7c4c3f0ae06aaf7f3bdf9f78bdfe042
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-10-17 12:17:50 +0200

    myMovies : module 5 : persistent API

--- exercises/module5/5.1/basic-api-boilerplate/app.js
+++ exercises/module5/5.1/basic-api-boilerplate/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const usersRouter = require('./routes/users');
-const pizzaRouter = require('./routes/pizzas');
+const filmsRouter = require('./routes/films');
 
 const app = express();
 
@@ -12,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/users', usersRouter);
-app.use('/pizzas', pizzaRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
