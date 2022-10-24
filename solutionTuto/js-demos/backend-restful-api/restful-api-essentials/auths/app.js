6d1d06972c902054ede9822e0ca219b3d454e220
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-10-24 12:26:28 +0200

    hmi-proxy

--- solutionTuto/js-demos/backend-restful-api/restful-api-essentials/auths/app.js
+++ solutionTuto/js-demos/backend-restful-api/restful-api-essentials/auths/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
+const usersRouter = require('./routes/users');
 const pizzaRouter = require('./routes/pizzas');
+const authsRouter = require('./routes/auths');
 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
+app.use('/users', usersRouter);
 app.use('/pizzas', pizzaRouter);
+app.use('/auths', authsRouter);
 
 module.exports = app;
