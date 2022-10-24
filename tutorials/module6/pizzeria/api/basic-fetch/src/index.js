2c3469275faeed735f8e081cc0a05aaf8c0aaabb
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-10-24 12:10:34 +0200

    api-cors

---
+++ tutorials/module6/pizzeria/api/basic-fetch/src/index.js
@@ -0,0 +1,9 @@
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
+import Router from './Components/Router/Router';
+
+Header();
+Navbar();
+Router();
+Footer();
